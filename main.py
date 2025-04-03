b655fe0186fa99b11c2c2656fb7b1047f1f8096b
Christian Albustin <christian.albustin@googlemail.com>
2025-04-03 15:37:23 +0200

    added splitting and killing asteroids

--- main.py
+++ main.py
@@ -40,6 +40,11 @@ def main():
             if asteroid.collides_with(player):
                 print("Game over!")
                 exit()
+                
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.split()
+                    shot.kill()
         
         screen.fill("black")
         
